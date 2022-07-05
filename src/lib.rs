3a528a4083934a0939b813627cdac8c5f000b29b
Frank Bell <fb@evilrobot.net>
2022-07-05 14:17:07 +0100

    Added next exercises

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use rand::seq::SliceRandom;
+    use std::collections::HashMap;
     use std::fmt::{Display, Formatter};
 
     #[test]
@@ -100,7 +101,62 @@ mod tests {
     }
 
     #[test]
-    fn exercise_6() {}
+    fn exercise_6() {
+        fn frequency(sentence: &str) -> HashMap<String, usize> {
+            sentence
+                .replace(".", "")
+                .replace(",", "")
+                .to_lowercase()
+                .split(" ")
+                .fold(HashMap::new(), |mut words, word| {
+                    *words.entry(word.to_string()).or_insert(0) += 1;
+                    words
+                })
+        }
+
+        println!(
+            "{:?}",
+            frequency("To be or not to be, that is the question.")
+        );
+        println!("{:?}", frequency("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Turpis tincidunt id aliquet risus feugiat in ante metus. Vel elit scelerisque mauris pellentesque pulvinar pellentesque. Malesuada fames ac turpis egestas maecenas pharetra. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique. Magna etiam tempor orci eu lobortis elementum nibh. Vestibulum sed arcu non odio euismod."));
+    }
+
+    #[test]
+    fn exercise_7() {
+        // • Write a generic struct Pair, which accepts two different types. Pair stores the two values in a field ‘first’ and a field ‘second’.
+        struct Pair<F: Copy, S: Copy> {
+            first: F,
+            second: S,
+        }
+
+        impl<F: Copy, S: Copy> Pair<F, S> {
+            // • Build a 'new' method for it
+            fn new(first: F, second: S) -> Self {
+                Self { first, second }
+            }
+
+            // • Build the get_left and get_right methods, which returns the first and second element of the Pair
+            fn get_left(&self) -> F {
+                self.first
+            }
+            fn get_right(&self) -> S {
+                self.second
+            }
+        }
+
+        // • Test the functions
+        let first = 1;
+        let second = 2.34;
+        let pair = Pair::new(first, second);
+        assert_eq!(first, pair.get_left());
+        assert_eq!(second, pair.get_right());
+
+        let first = 10.23;
+        let second = 18u8;
+        let pair = Pair::new(first, second);
+        assert_eq!(first, pair.get_left());
+        assert_eq!(second, pair.get_right());
+    }
 
     // #[test]
     // fn iterator_question() {
